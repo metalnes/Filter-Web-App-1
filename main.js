d75f5058aa683a2a02d62e8ddabfe178305d5661
metalnes <98011602+metalnes@users.noreply.github.com>
2023-06-23 13:39:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
-function preload(){
+nosex=0;
+nosey=0;
 
+function preload(){
+img=loadImage("m.png");
 }
 function setup(){
-    canvas=createCanvas(600,400)
-    canvas.center()
+    canvas=createCanvas(500,500);
+    canvas.center();
+video=createCapture(VIDEO);
+video.hide();
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on("pose",gotPoses);
 }
 function draw(){
-
+image(video,0,0 ,500,500);
+image(img,nosex,nosey,80,40);
 }
 
 function take_snapshot(){
     save("itisfiltered")
+}
+function modelLoaded(){
+    console.log("poseNet is initialized");
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        nosex=results[0].pose.nose.x-100;
+        nosey=results[0].pose.nose.y+35;
+    }
 }
